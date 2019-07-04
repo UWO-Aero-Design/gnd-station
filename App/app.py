ed3d745d45a462f5d1aae826361d134279ddb2bd
Andrew Ning <aning97@gmail.com>
2019-07-04 01:57:27 -0400

    Added basic UI

--- App/app.py
+++ App/app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 import socket
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/assets/Icons')
 
 @app.route("/")
-def server():
-    return 'Hello'
+def home():
+    #hello
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
