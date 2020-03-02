3eb3c70b64122a597c689b53b9703b25f54b98ec
Andrew Ning <aning97@gmail.com>
2020-03-02 14:08:45 -0500

    Add websocket channels for each type of data

--- App/backend/app/serial/events.py
+++ App/backend/app/serial/events.py
@@ -34,82 +34,150 @@ def post_serial_read(app,data = None):
     global point
     point += 1
 
-    #Database insertions
-    #All database access should be inside app context since this is running in a background thread
-    # with app.app_context():
-    #     databaseObj = databasehelperclass.pointtable(1,point)
-    #     databaseinsertion(databaseObj)
-
     IMUData = data[1]
     print(IMUData)
-    #     databaseObj = databasehelperclass.imuvaluestable(float(IMUData.ax),float(IMUData.ay),float(IMUData.az),
-    #         float(IMUData.yaw),float(IMUData.pitch),float(IMUData.roll),
-    #         float(IMUData.mx),float(IMUData.my),float(IMUData.mz),
-    #         float(IMUData.gx),float(IMUData.gy),float(IMUData.gz),
-    #         1,point)
-    #     databaseinsertion(databaseObj)
 
     GPSData = data[2]
     print(GPSData)
-    #     databaseObj = databasehelperclass.gpsvaluetable(float(GPSData.lat),float(GPSData.lon),float(GPSData.speed),
-    #         float(GPSData.satellites),float(GPSData.altitude),float(GPSData.time),
-    #         int(GPSData.date),1,point)
-    #     databaseinsertion(databaseObj)
 
     EnviroData = data[3]
     print(EnviroData)
-    #     databaseObj = databasehelperclass.environmentalsensortable(float(EnviroData.pressure),
-    #         float(EnviroData.humidity),
-    #         float(EnviroData.temperature),
-    #         1,point)
-    #     databaseinsertion(databaseObj)
 
     BatteryData = data[4]
     print(BatteryData)
-    #     databaseObj = databasehelperclass.batterystatustable(float(BatteryData.voltage),
-    #         float(BatteryData.current),
-    #         1,point)
-    #     databaseinsertion(databaseObj)
 
     StatusData = data[6]
     print(StatusData)
-    #     databaseObj = databasehelperclass.systemstatustable(float(StatusData.rssi),
-    #         float(StatusData.state),
-    #         1,point)
-    #     databaseinsertion(databaseObj)
 
     ServoData = data[7]
     print(ServoData)
-    #     databaseObj = databasehelperclass.servodatatable(float(ServoData.servo0),
-    #         float(ServoData.servo1),
-    #         float(ServoData.servo2),
-    #         float(ServoData.servo3),
-    #         float(ServoData.servo4),
-    #         float(ServoData.servo5),
-    #         float(ServoData.servo6),
-    #         float(ServoData.servo7),
-    #         float(ServoData.servo8),
-    #         float(ServoData.servo9),
-    #         float(ServoData.servo10),
-    #         float(ServoData.servo11),
-    #         float(ServoData.servo12),
-    #         float(ServoData.servo13),
-    #         float(ServoData.servo14),
-    #         float(ServoData.servo15),
-    #         1,point)
-    #     databaseinsertion(databaseObj)
 
-    #if IMUData:
-    #    jsonData = {'Accel X':IMUData.AccelX,
-    #                'Accel Y':IMUData.AccelY}
+    #Database insertions and websocket messages
+    #All database access should be inside app context since this is running in a background thread
+    with app.app_context():
+        databaseObj = databasehelperclass.pointtable(1,point)
+        databaseinsertion(databaseObj)
 
-    print(IMUData[1])
+        if IMUData is not None:
+            jsonData = {'ax':IMUData.ax,
+                        'ay':IMUData.ay,
+                        'az':IMUData.az,
+                        'gx':IMUData.gx,
+                        'gy':IMUData.gy,
+                        'gz':IMUData.gz,
+                        'mx':IMUData.mx,
+                        'my':IMUData.my,
+                        'mz':IMUData.mz,
+                        'yaw':IMUData.yaw,
+                        'pitch':IMUData.pitch,
+                        'roll':IMUData.roll}
+            # print(jsonData)
+            socketio.emit('IMUChannel',jsonData)
+            socketio.emit('connectStatus','Connected')
 
-    #Pass to frontend
-    jsonData = {'lat':GPSData.lat + random.randint(-1000,1000),'lon':GPSData.lon + random.randint(-1000,1000),'altitude':GPSData.altitude + random.randint(-1000,1000),'speed':GPSData.speed + random.randint(-1000,1000),'time':GPSData.time + random.randint(-1000,1000),'satellites':GPSData.satellites + random.randint(-1000,1000),'date':GPSData.date + random.randint(-1000,1000)}
-    print(jsonData)
-    socketio.emit('dataChannel',jsonData)
-    socketio.emit('connectStatus','Connected')
+            databaseObj = databasehelperclass.imuvaluestable(float(IMUData.ax),float(IMUData.ay),float(IMUData.az),
+                float(IMUData.yaw),float(IMUData.pitch),float(IMUData.roll),
+                float(IMUData.mx),float(IMUData.my),float(IMUData.mz),
+                float(IMUData.gx),float(IMUData.gy),float(IMUData.gz),
+                1,point)
+            databaseinsertion(databaseObj)
+
+
+        if GPSData is not None:
+            jsonData = {'lat':GPSData.lat,
+                        'lon':GPSData.lon,
+                        'altitude':GPSData.altitude,
+                        'speed':GPSData.speed,
+                        'time':GPSData.time,
+                        'satellites':GPSData.satellites,
+                        'date':GPSData.date}
+            # print(jsonData)
+            socketio.emit('GPSChannel',jsonData)
+            socketio.emit('connectStatus','Connected')
+
+            databaseObj = databasehelperclass.gpsvaluetable(float(GPSData.lat),float(GPSData.lon),float(GPSData.speed),
+                float(GPSData.satellites),float(GPSData.altitude),float(GPSData.time),
+                int(GPSData.date),1,point)
+            databaseinsertion(databaseObj)
+
+        if EnviroData is not None:
+            jsonData = {'pressure':EnviroData.pressure,
+                        'humidity':EnviroData.humidity,
+                        'temperature':EnviroData.temperature}
+            # print(jsonData)
+            socketio.emit('EnviroChannel',jsonData)
+            socketio.emit('connectStatus','Connected')
+
+            databaseObj = databasehelperclass.environmentalsensortable(float(EnviroData.pressure),
+                float(EnviroData.humidity),
+                float(EnviroData.temperature),
+                1,point)
+            databaseinsertion(databaseObj)
+
+        if BatteryData is not None:
+            jsonData = {'voltage':BatteryData.voltage,
+                        'current':BatteryData.current}
+            # print(jsonData)
+            socketio.emit('BatteryChannel',jsonData)
+            socketio.emit('connectStatus','Connected')
+
+            databaseObj = databasehelperclass.batterystatustable(float(BatteryData.voltage),
+                float(BatteryData.current),
+                1,point)
+            databaseinsertion(databaseObj)
+
+        if StatusData is not None:
+            jsonData = {'rrsi':StatusData.rssi,
+                        'state':StatusData.state}
+            # print(jsonData)
+            socketio.emit('StatusChannel',jsonData)
+            socketio.emit('connectStatus','Connected')
+
+            databaseObj = databasehelperclass.systemstatustable(float(StatusData.rssi),
+                float(StatusData.state),
+                1,point)
+            databaseinsertion(databaseObj)
+
+        if ServoData is not None:
+            jsonData = {'servo0':ServoData.servo0,
+                        'servo1':ServoData.servo1,
+                        'servo2':ServoData.servo2,
+                        'servo3':ServoData.servo3,
+                        'servo4':ServoData.servo4,
+                        'servo5':ServoData.servo5,
+                        'servo6':ServoData.servo6,
+                        'servo7':ServoData.servo7,
+                        'servo8':ServoData.servo8,
+                        'servo9':ServoData.servo9,
+                        'servo10':ServoData.servo10,
+                        'servo11':ServoData.servo11,
+                        'servo12':ServoData.servo12,
+                        'servo13':ServoData.servo13,
+                        'servo14':ServoData.servo14,
+                        'servo15':ServoData.servo15}
+            # print(jsonData)
+            socketio.emit('ServoChannel',jsonData)
+            socketio.emit('connectStatus','Connected')
+
+            databaseObj = databasehelperclass.servodatatable(float(ServoData.servo0),
+                float(ServoData.servo1),
+                float(ServoData.servo2),
+                float(ServoData.servo3),
+                float(ServoData.servo4),
+                float(ServoData.servo5),
+                float(ServoData.servo6),
+                float(ServoData.servo7),
+                float(ServoData.servo8),
+                float(ServoData.servo9),
+                float(ServoData.servo10),
+                float(ServoData.servo11),
+                float(ServoData.servo12),
+                float(ServoData.servo13),
+                float(ServoData.servo14),
+                float(ServoData.servo15),
+                1,point)
+            databaseinsertion(databaseObj)
+
     #time.sleep(0.1)
     # The plan is here to take data that is parsed from the serial port and add it to the DB
 
--- App/frontend/src/components/MapComponents/MapInfo.vue
+++ App/frontend/src/components/MapComponents/MapInfo.vue
@@ -31,7 +31,7 @@
       connect() {
       this.isConnected = true;
     },
-    dataChannel: function(data) {
+    GPSChannel: function(data) {
       this.GPSData = data;
     }
     }
