2f6aa44905aa75cddd13c57bb97de340864dd0f6
Jeff St. Jean <jeffrey@stjean.ca>
2021-01-20 16:53:38 -0500

    Support multiple commands

--- api/routes/CommandRoute.js
+++ api/routes/CommandRoute.js
@@ -2,46 +2,50 @@ const router = require('express').Router();
 const { Message } = require('../message/message_pb')
 const usb = require('../config/usb')
 
-// temp
 let packet_number = 0;
 
 router.post('/', (req, res) => { 
-    if(!req.body.command) {
+    let commands = req.body.commands;
+    if(!commands) {
         return res.status(400).send('No command specified')
     }
-    const command = req.body.command.toUpperCase();
     let message = new Message()
     message.setSender(Message.Location.GROUND_STATION)
     message.setRecipient(Message.Location.PLANE)
     message.setPacketNumber(packet_number++);
     message.setTime(Math.floor(new Date().getTime() / 1000));
     message.setStatus(Message.Status.READY);
-    if(command === 'OPEN_DOORS') {
-        const serialized_message = message.serializeBinary()
-        let hex_string = '';
-        for(var i = 0; i < serialized_message.length; i++) {
-            hex_string = hex_string + serialized_message[i].toString(16) + " "
+    commands.forEach(command => {
+        command = command.toUpperCase();
+        if(command === 'OPEN_DOORS') {
+            message.addCommands(Message.Command.OPEN_DOORS);
         }
-        usb.write(serialized_message)
-            .then(() => {
-                return res.status(200).send('Opened doors')
-            })
-            .catch(error => {
-                return res.status(500).json(error);
-            })
+        else if(command === 'CLOSE_DOORS') {
+            message.addCommands(Message.Command.CLOSE_DOORS);
+        }
+        else if(command === 'DROP_PAYLOAD') {
+            message.addCommands(Message.Command.DROP_PAYLOADS);
+        }
+        else if(command === 'DROP_GLIDERS') {
+            message.addCommands(Message.Command.DROP_GLIDERS);
+        }
+        else {
+            return res.status(406).send('Invalid command')
+        }
+    })
+    const serialized_message = message.serializeBinary()
+    let hex_string = '';
+    for(var i = 0; i < serialized_message.length; i++) {
+        hex_string = hex_string + serialized_message[i].toString(16) + " "
     }
-    else if(command === 'CLOSE_DOORS') {
-        return res.status(200).send('Closed doors')
-    }
-    else if(command === 'DROP_PAYLOAD') {
-        return res.status(200).send('Dropped payload')
-    }
-    else if(command === 'DROP_GLIDERS') {
-        return res.status(200).send('Dropped gliders')
-    }
-    else {
-        return res.status(406).send('Invalid command')
-    }
+    console.log(`Serialized message to ${serialized_message.length} bytes`)
+    usb.write(serialized_message)
+        .then(() => {
+            return res.status(200).send('Packet sent')
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        })
 });
 
 
