46bb3309a7aa77f43ae1a23a634e630679ad1706
zach1590 <zdsouza4@uwo.ca>
2022-01-11 23:08:46 -0500

    Add 2 new tests
    Made first test proper
    Second and third test looks for a failure and for an empty array

--- api/tests/GroundRoute.test.js
+++ api/tests/GroundRoute.test.js
@@ -7,17 +7,82 @@ const usb = require('../config/usb')
 jest.mock('../config/usb');
 const SerialPort = require('serialport'); 
 
-const app = new express();
+const app = express();
 app.use('/', router);
 
 describe('Ground Routes', function () {
 
-    usb.list = () => {
-        return SerialPort.list();   // Ideally replace this with something manmade
-    }
-
-    test('responds to /com', async () => {
+    test('responds to /com with status 200', async () => {
+        usb.list = () => {
+            return Promise.resolve([
+                {
+                    path: '/dev/ttyACM0',
+                    manufacturer: 'Arduino (www.arduino.cc)',
+                    serialNumber: '752303138333518011C1',
+                    pnpId: 'usb-Arduino__www.arduino.cc__0043_752303138333518011C1-if00',
+                    locationId: undefined,
+                    productId: '0043',
+                    vendorId: '2341'
+                },
+                {
+                    path: '/dev/ttyACM1',
+                    manufacturer: 'Arduino (www.arduino.cc)',
+                    serialNumber: '752303138333518011C1',
+                    pnpId: 'usb-Arduino__www.arduino.cc__0043_752303138333518011C1-if00',
+                    locationId: undefined,
+                    productId: '0044',
+                    vendorId: '2341'
+                },
+                { manufacturer: undefined,
+                    serialNumber: undefined,
+                    pnpId: undefined,
+                    locationId: undefined,
+                    vendorId: undefined,
+                    productId: undefined,
+                    path: '/dev/ttyS0' 
+                }
+            ]);
+        }
         const res = await request(app).get('/com');
+        expect(res.body).toEqual([
+            {
+                path: '/dev/ttyACM0',
+                manufacturer: 'Arduino (www.arduino.cc)',
+                serialNumber: '752303138333518011C1',
+                pnpId: 'usb-Arduino__www.arduino.cc__0043_752303138333518011C1-if00',
+                locationId: undefined,
+                productId: '0043',
+                vendorId: '2341'
+            },
+            {
+                path: '/dev/ttyACM1',
+                manufacturer: 'Arduino (www.arduino.cc)',
+                serialNumber: '752303138333518011C1',
+                pnpId: 'usb-Arduino__www.arduino.cc__0043_752303138333518011C1-if00',
+                locationId: undefined,
+                productId: '0044',
+                vendorId: '2341'
+            },
+        ])
         expect(res.statusCode).toBe(200);
     });
+
+    test('responds to /com with status 500', async () => {
+        usb.list = () => {
+            return Promise.reject([]);
+        }
+        const res = await request(app).get('/com');
+        expect(res.statusCode).toBe(500);
+    });
+
+    test('responds to /com with status 500', async () => {
+        usb.list = () => {
+            return Promise.resolve([]);
+        }
+        const res = await request(app).get('/com');
+        expect(res.body).toEqual([]);
+        expect(res.statusCode).toBe(200);   // Should this be 200
+    });
 });
+
+// let get_all = req.query.all;         // What does this do
