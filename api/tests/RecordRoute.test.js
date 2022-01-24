d61835375168c87b19de4a61a73b53c6c2af99d8
zach1590 <zdsouza4@uwo.ca>
2022-01-23 23:36:44 -0500

    Add new post request tests
    Test for posting a record_id
    Test where we give no action in the request body

--- api/tests/RecordRoute.test.js
+++ api/tests/RecordRoute.test.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = require('../routes/RecordRoute.js');
 
 const app = express();
-app.use('/', router);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded());
+app.use('/', router);
 
 const Recording = require('../models/RecordingModel')
 jest.mock('../models/RecordingModel');
@@ -14,7 +14,7 @@ describe('Record Routes Get', function () {
     
     test('if we are recording true with 200', async () => {
         Recording.is_recording = () => {
-            return Promise.resolve(true);
+            return true;
         }
         const res = await request(app).get('/status')
         expect(res.statusCode).toBe(200);
@@ -23,10 +23,25 @@ describe('Record Routes Get', function () {
 
     test('if we are recording false with 200', async () => {
         Recording.is_recording = () => {
-            return Promise.resolve(false);
+            return false;
         }
         const res = await request(app).get('/status')
         expect(res.statusCode).toBe(200);
         expect(res.body.status).toEqual(false)
     });
+});
+
+describe('Not really unit tests', function () {
+    
+    test('post record id', async () => {
+        const res = await request(app).post('/1');
+        expect(res.statusCode).toBe(200);
+        expect(res.body.id).toEqual(1);
+    });
+
+    test('post with no action expects 400 back', async () => {
+        const res = await request(app).post('/').send({action: ""});
+        expect(res.statusCode).toBe(400);
+    });
+
 });
