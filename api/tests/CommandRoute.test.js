bf0ac9e3095e7646c1f50cbc8fcfe116bfcc4952
zach <zdsouza4@uwo.ca>
2022-03-26 18:49:23 -0400

    clean up tests

--- api/tests/CommandRoute.test.js
+++ api/tests/CommandRoute.test.js
@@ -14,21 +14,30 @@ describe('Command Routes', function () {
         expect(res.error.text).toEqual('No command specified');
     });
 
-    // test('Give body that is not a list', async () => {
-    //     const res = await request(app).post('/').send({commands: "OPEN_DOORS"});
-    //     expect(res.statusCode).toBe(400);
-    //     expect(res.error.text).toEqual('Invalid command');
-    // });
+    // Fails because 500 is received instead but commandroute doesnt seem to ever return 500
+    test('Give body that is valid but not in a list', async () => {
+        const res = await request(app).post('/').send({commands: "OPEN_DOORS"});
+        expect(res.statusCode).toBe(406);
+        expect(res.error.text).toEqual('Invalid command');
+    });
 
-    // test('Give invalid body that is not a list', async () => {
-    //     const res = await request(app).post('/').send({commands: "lalalalalal"});
-    //     expect(res.statusCode).toBe(400);
-    //     expect(res.error.text).toEqual('Invalid command');
-    // });
+    // Fails because idk
+    test('Give body that is valid in a list', () => {
+        console.log = jest.fn();
+        request(app).post('/').send({commands: ["OPEN_DOORS"]});
+        expect(console.log.mock.calls[0][0]).stringContaining('Serialized message');
+    });
+
+    // Fails because 500 is received instead but commandroute doesnt seem to ever return 500
+    test('Give invalid body that is not a list', async () => {
+        const res = await request(app).post('/').send({commands: "lalalalalal"});
+        expect(res.statusCode).toBe(406);
+        expect(res.error.text).toEqual('Invalid command');
+    });
 
     test('Give invalid body that is in a list', async () => {
         const res = await request(app).post('/').send({commands: ["lalalalalal"]});
         expect(res.statusCode).toBe(406);
-        expect(res.error).toEqual('Invalid command');
+        expect(res.error.text).toEqual('Invalid command');
     });
 });
