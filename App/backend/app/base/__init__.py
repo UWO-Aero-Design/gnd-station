7ad0245cf3a20e47e98dff8b84c96a3274a613c4
Jeff St. Jean <jeffrey@stjean.ca>
2022-04-11 19:48:37 -0400

    removed unecessary files and folders from before

--- App/backend/app/base/__init__.py
+++
@@ -1,7 +0,0 @@
-#API endpoints for base view
-
-from flask import Blueprint
-
-api = Blueprint('base',__name__)
-
-from app.base import routes
--- App/backend/app/info/__init__.py
+++
@@ -1,7 +0,0 @@
-#API endpoints for info view
-
-from flask import Blueprint
-
-api = Blueprint('info',__name__)
-
-from app.info import routes
