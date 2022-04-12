1a8dd8208824a5ff53d574918d2fe86a81420708
Jeff St. Jean <jeffrey@stjean.ca>
2022-04-11 20:15:40 -0400

    small bug fixes in the data interface

--- api/data_interface/data_interface.js
+++ api/data_interface/data_interface.js
@@ -1,48 +1,67 @@
 const USB_DRIVER = require('./usb_driver')
 const DUMMY_DRIVER = require('./dummy_driver')
 
+const PRINT_TELEMETRY_MESSAGES = true;
+const PRINT_DRIVER_MESSAGES = true;
+
 /* all drivers should have the following:
  *  - a name string property to be used for driver selection
  *  - a init() function to perform any required startup routines
  *  - a process_command(command, args) function that will handle inbound commands from the frontend
  *  - a event emitter called event with the following event channels
- *      - telemetry: for data to be sent to the frontend
- *      - command: for data to be sent to the aircraft
- *      - error: for any errors that occur
+ *      - telemetry: for data to be sent to the frontend -> { data: { ... } }
+ *      - command: for data to be sent to the aircraft -> { command: '', args: { ... } }
+ *      - error: for any errors that occur -> { error: '', message: '' }
  */
-const drivers = [
-    USB_DRIVER,
-    DUMMY_DRIVER
-]
+const DEFAULT_DRIVER = DUMMY_DRIVER;
+const drivers = [ USB_DRIVER, DUMMY_DRIVER ];
 let current_driver;
-const DEFAULT_DRIVER = 'DUMMY_DRIVER';
+
 
 const init = () => {
-    current_driver = select_driver('USB_DRIVER');
+    current_driver = DEFAULT_DRIVER;
 
     drivers.forEach(driver => {
+        // run the init function for each driver
         driver.init();
-        driver.event.on('telemetry', (data) => {
-            console.log(`Received telemetry from ${driver.name}: ${data.message}`)
+
+        // set up event handling on each driver
+        // event: { data: { ... } }
+        driver.event.on('telemetry', (event) => {
+            const data = JSON.parse(event).data
+            if(PRINT_TELEMETRY_MESSAGES) {
+                console.log(`Received telemetry from ${driver.name} (packet #: ${data.packet_number})`)
+            }
+
+            // only run code for the selected driver
             if(driver.name === current_driver.name) {
                 // TODO: send to web socket module (to frontend)
             }
         })
 
-        driver.event.on('error', (data) => {
-            console.log(`Received error from ${driver.name}: ${data.message}`)
+        // event: { error: '', message: '' }
+        driver.event.on('error', (event) => {
+            event = JSON.parse(event)
+            console.log(`Received error from ${driver.name}: ${event.error} - ${event.message}`)
         })
     })
 }
 
 const select_driver = (driver_name) => {
-    driver_name = driver_name.toUpperCase()
+    // to hold the return value
+    let selected_driver = null;
+
     drivers.forEach(driver => {
-        if(driver.name === driver_name) {
-            console.log(`switching driver to ${driver.name}`)
-            current_driver = driver;
+        // check if the current driver is the desired driver
+        if(driver.name.toLowerCase() === driver_name.toLowerCase()) {
+            if(PRINT_DRIVER_MESSAGES) {
+                console.log(`Selecting data driver ${driver.name}`)
+            }
+            selected_driver = driver;
         }
     })
+
+    return selected_driver;
 }
 
 const get_driver = () => {
