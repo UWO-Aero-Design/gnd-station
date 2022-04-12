8ae351d36e918b0f5ea5b11947d756852e1b5c3a
Jeff St. Jean <jeffrey@stjean.ca>
2022-04-11 22:53:28 -0400

    removed debug line

--- api/routes/CommandRoute.js
+++ api/routes/CommandRoute.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const data_interface = require('../services/data_interface/data_interface')
 
 const Recording = require('../models/RecordingModel')
 const { Message } = require('../message/message_pb')
@@ -11,66 +12,12 @@ router.post('/', (req, res) => {
     if(!commands) {
         return res.status(400).send('No command specified')
     }
-    // build the message and include meta data
-    let message = new Message()
-    message.setSender(Message.Location.GROUND_STATION)
-    message.setRecipient(Message.Location.PLANE)
-    message.setPacketNumber(packet_number++);
-    message.setTime(Math.floor(new Date().getTime() / 1000));
-    message.setStatus(Message.Status.READY);
 
-    // check if each command exists and add it
-    commands.forEach(command => {
-        command = command.toUpperCase();
-        if(command === 'OPEN_DOORS') {
-            message.addCommands(Message.Command.OPEN_DOORS);
-        }
-        else if(command === 'CLOSE_DOORS') {
-            message.addCommands(Message.Command.CLOSE_DOORS);
-        }
-        else if(command === 'DROP_PAYLOAD') {
-            message.addCommands(Message.Command.DROP_PAYLOADS);
-        }
-        else if(command === 'DROP_GLIDERS') {
-            message.addCommands(Message.Command.DROP_GLIDERS);
-        }
-        else {
-            return res.status(406).send('Invalid command')
-        }
+    commands.forEach(command_item => {
+        data_interface.process_command(command_item.command, command_item.args)
     })
 
-    // add to database (if necessary)
-    if(Recording.is_recording()) {
-        Recording.findById(Recording.get_current_recording())
-            .then(recording => {
-                recording.commands.push(message.toObject());
-                recording.save()
-                    .then(() => {
-                        console.log('Saved command')
-                    })
-                    .catch(error => {
-                        console.log('Unable to save command');
-                        console.log(error);
-                    });
-            })
-            .catch(error => {
-                console.log('Unable to find recording');
-                console.log(error);
-            })
-    }
-
-    // serialize
-    const serialized_message = message.serializeBinary()
-    console.log(`Serialized message to ${serialized_message.length} bytes`)
-
-    // send
-    // usb.write(serialized_message)
-    //     .then(() => {
-    //         return res.status(200).send('Packet sent')
-    //     })
-    //     .catch(error => {
-    //         return res.status(500).json(error);
-    //     })
+    return res.status(200).send();
 });
 
 
